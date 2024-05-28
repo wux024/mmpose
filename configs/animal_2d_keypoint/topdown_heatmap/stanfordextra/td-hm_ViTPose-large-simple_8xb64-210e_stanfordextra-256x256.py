8671a89da8dd2d28321afb23e3ce9c4e26598af6
Adam Wiveion <wux024@nenu.edu.cn>
2024-05-28 09:11:54 +0800

    update

--- configs/animal_2d_keypoint/topdown_heatmap/stanfordextra/td-hm_ViTPose-large-simple_8xb64-210e_stanfordextra-256x256.py
+++ configs/animal_2d_keypoint/topdown_heatmap/stanfordextra/td-hm_ViTPose-large-simple_8xb64-210e_stanfordextra-256x256.py
@@ -76,7 +76,7 @@ model = dict(
     head=dict(
         type='HeatmapHead',
         in_channels=1024,
-        out_channels=15,
+        out_channels=20,
         deconv_out_channels=[],
         deconv_kernel_sizes=[],
         final_layer=dict(kernel_size=3, padding=1),
