41c993ba886fe7e6bafaac60a433ff8346f40cc2
Adam Wiveion <wux024@nenu.edu.cn>
2024-06-05 08:30:55 +0800

    update

--- configs/animal_2d_keypoint/rtmo/pups/rtmo-m_16xb16-600e_pups-640x640.py
+++ configs/animal_2d_keypoint/rtmo/pups/rtmo-m_16xb16-600e_pups-640x640.py
@@ -172,17 +172,11 @@ val_evaluator = [dict(type='CocoMetric',
                       ann_file=data_root + 'annotations/val.json', 
                       score_mode='bbox',
                       nms_mode='none'),
-                      dict(type='PCKAccuracy', thr=0.2),
-                      dict(type='AUC'),
-                      dict(type='EPE')
                       ]
 test_evaluator = [dict(type='CocoMetric', 
                       ann_file=data_root + 'annotations/test.json', 
                       score_mode='bbox',
                       nms_mode='none'),
-                      dict(type='PCKAccuracy', thr=0.2),
-                      dict(type='AUC'),
-                      dict(type='EPE')
                       ]
 
 # hooks
