f2a6c4439335d244e05a2a33ac9a054661e3acb1
Adam Wiveion <wux024@nenu.edu.cn>
2024-06-24 14:22:50 +0800

    update

--- configs/animal_2d_keypoint/topdown_heatmap/horse10/td-hm_ViTPose-huge-simple_8xb64-210e_horse10-256x256.py
+++ configs/animal_2d_keypoint/topdown_heatmap/horse10/td-hm_ViTPose-huge-simple_8xb64-210e_horse10-256x256.py
@@ -123,7 +123,7 @@ train_dataloader = dict(
         data_root=data_root,
         data_mode=data_mode,
         ann_file='annotations/train.json',
-        data_prefix=dict(img='images/train/'),
+        data_prefix=dict(img=''),
         pipeline=train_pipeline,
     ))
 val_dataloader = dict(
@@ -137,7 +137,7 @@ val_dataloader = dict(
         data_root=data_root,
         data_mode=data_mode,
         ann_file='annotations/val.json',
-        data_prefix=dict(img='images/val/'),
+        data_prefix=dict(img=''),
         test_mode=True,
         pipeline=val_pipeline,
     ))
@@ -152,7 +152,7 @@ test_dataloader = dict(
         data_root=data_root,
         data_mode=data_mode,
         ann_file='annotations/test.json',
-        data_prefix=dict(img='images/test'),
+        data_prefix=dict(img=''),
         test_mode=True,
         pipeline=val_pipeline,
     ))
