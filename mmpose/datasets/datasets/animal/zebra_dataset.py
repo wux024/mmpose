f7e27200a21c3e3f1f7ba470c232503ccc0474ab
Adam Wiveion <wux024@nenu.edu.cn>
2024-06-25 12:00:02 +0800

    update

--- mmpose/datasets/datasets/animal/zebra_dataset.py
+++ mmpose/datasets/datasets/animal/zebra_dataset.py
@@ -69,48 +69,48 @@ class ZebraDataset(BaseCocoStyleDataset):
 
     METAINFO: dict = dict(from_file='configs/_base_/datasets/zebra.py')
 
-    def parse_data_info(self, raw_data_info: dict) -> Optional[dict]:
-        """Parse raw Zebra annotation of an instance.
+    # def parse_data_info(self, raw_data_info: dict) -> Optional[dict]:
+    #     """Parse raw Zebra annotation of an instance.
 
-        Args:
-            raw_data_info (dict): Raw data information loaded from
-                ``ann_file``. It should have following contents:
+    #     Args:
+    #         raw_data_info (dict): Raw data information loaded from
+    #             ``ann_file``. It should have following contents:
 
-                - ``'raw_ann_info'``: Raw annotation of an instance
-                - ``'raw_img_info'``: Raw information of the image that
-                    contains the instance
+    #             - ``'raw_ann_info'``: Raw annotation of an instance
+    #             - ``'raw_img_info'``: Raw information of the image that
+    #                 contains the instance
 
-        Returns:
-            dict: Parsed instance annotation
-        """
+    #     Returns:
+    #         dict: Parsed instance annotation
+    #     """
 
-        ann = raw_data_info['raw_ann_info']
-        img = raw_data_info['raw_img_info']
+    #     ann = raw_data_info['raw_ann_info']
+    #     img = raw_data_info['raw_img_info']
 
-        img_path = osp.join(self.data_prefix['img'], img['file_name'])
+    #     img_path = osp.join(self.data_prefix['img'], img['file_name'])
 
-        # get bbox in shape [1, 4], formatted as xywh
-        # use the entire image which is 160x160
-        bbox = np.array([0, 0, 160, 160], dtype=np.float32).reshape(1, 4)
+    #     # get bbox in shape [1, 4], formatted as xywh
+    #     # use the entire image which is 160x160
+    #     bbox = np.array([0, 0, 160, 160], dtype=np.float32).reshape(1, 4)
 
-        # keypoints in shape [1, K, 2] and keypoints_visible in [1, K]
-        _keypoints = np.array(
-            ann['keypoints'], dtype=np.float32).reshape(1, -1, 3)
-        keypoints = _keypoints[..., :2]
-        keypoints_visible = np.minimum(1, _keypoints[..., 2])
+    #     # keypoints in shape [1, K, 2] and keypoints_visible in [1, K]
+    #     _keypoints = np.array(
+    #         ann['keypoints'], dtype=np.float32).reshape(1, -1, 3)
+    #     keypoints = _keypoints[..., :2]
+    #     keypoints_visible = np.minimum(1, _keypoints[..., 2])
 
-        num_keypoints = ann['num_keypoints']
+    #     num_keypoints = ann['num_keypoints']
 
-        data_info = {
-            'img_id': ann['image_id'],
-            'img_path': img_path,
-            'bbox': bbox,
-            'bbox_score': np.ones(1, dtype=np.float32),
-            'num_keypoints': num_keypoints,
-            'keypoints': keypoints,
-            'keypoints_visible': keypoints_visible,
-            'iscrowd': ann['iscrowd'],
-            'id': ann['id'],
-        }
+    #     data_info = {
+    #         'img_id': ann['image_id'],
+    #         'img_path': img_path,
+    #         'bbox': bbox,
+    #         'bbox_score': np.ones(1, dtype=np.float32),
+    #         'num_keypoints': num_keypoints,
+    #         'keypoints': keypoints,
+    #         'keypoints_visible': keypoints_visible,
+    #         'iscrowd': ann['iscrowd'],
+    #         'id': ann['id'],
+    #     }
 
-        return data_info
+    #     return data_info
