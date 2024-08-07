017cb85494b0f1719127347cfac8874a0b4bc7a8
Adam Wiveion <wux024@nenu.edu.cn>
2024-08-07 10:21:28 +0800

    update

--- tools/train_apt36k_imgsz_rt.sh
+++ tools/train_apt36k_imgsz_rt.sh
@@ -11,7 +11,7 @@ while [[ $# -gt 0 ]]; do
             imgsz="$2"
             shift 2
             ;;
-        *
+        *)
             echo "Unknown parameter passed: $1"
             exit 1
             ;;
