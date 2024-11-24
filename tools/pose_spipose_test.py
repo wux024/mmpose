b52a0ad95eda70132f8e1426a02ed92e193f88a3
Adam Wiveion <wux024@nenu.edu.cn>
2024-11-24 14:20:51 +0800

    update

---
+++ tools/pose_spipose_test.py
@@ -0,0 +1,206 @@
+import argparse
+import os
+import subprocess
+
+
+def prepare_configurations(mode, dataset_name):
+
+    configurations = []
+
+    if mode == 'bottomup':
+        print("Preparing for bottomup processing...")
+        configurations.extend([
+            f"cid_hrnet-w32_8xb32-140e_{dataset_name}-512x512.py",
+            f"cid_hrnet-w48_8xb32-140e_{dataset_name}-512x512.py",
+            f"dekr_hrnet-w32_8xb32-140e_{dataset_name}-512x512.py",
+            f"dekr_hrnet-w48_8xb32-140e_{dataset_name}-640x640.py"
+        ])
+    elif mode == 'rtmo':
+        print("Preparing for RTMO processing...")
+        configurations.extend([
+            f"{mode}-s_8xb16-600e_{dataset_name}-640x640.py",
+            f"{mode}-m_16xb16-600e_{dataset_name}-640x640.py",
+            f"{mode}-l_16xb16-600e_{dataset_name}-640x640.py"
+        ])
+    elif mode == 'rtmpose':
+        print("Preparing for RTMPose processing...")
+        configurations.extend([
+            f"{mode}-t_8xb256-420e_{dataset_name}-256x256.py",
+            f"{mode}-s_8xb256-420e_{dataset_name}-256x256.py",
+            f"{mode}-m_8xb256-420e_{dataset_name}-256x256.py",
+            f"{mode}-l_8xb256-420e_{dataset_name}-256x256.py"
+        ])
+    elif mode == 'topdown_heatmap':
+        print("Preparing for topdown heatmap processing...")
+        configurations.extend([
+            f"td-hm_res50_8xb64-210e_{dataset_name}-256x256.py",
+            f"td-hm_res101_8xb64-210e_{dataset_name}-256x256.py",
+            f"td-hm_res152_8xb32-210e_{dataset_name}-256x256.py",
+            f"td-hm_hrnet-w32_8xb64-210e_{dataset_name}-256x256.py",
+            f"td-hm_hrnet-w48_8xb64-210e_{dataset_name}-256x256.py"
+        ])
+    elif mode == 'topdown_others':
+        print("Preparing for topdown regression processing...")
+        configurations.extend([
+            f"ipr_res50_8xb64-210e_{dataset_name}-256x256.py",
+            f"simcc_res50_8xb64-210e_{dataset_name}-256x256.py",
+            f"td-reg_res50_8xb64-210e_{dataset_name}-256x256.py",
+            f"td-reg_res50_rle-8xb64-210e_{dataset_name}-256x256.py",
+            f"td-reg_res101_8xb64-210e_{dataset_name}-256x256.py",
+            f"td-reg_res101_rle-8xb64-210e_{dataset_name}-256x256.py",
+            f"td-reg_res152_8xb64-210e_{dataset_name}-256x256.py",
+            f"td-reg_res152_rle-8xb64-210e_{dataset_name}-256x256.py"
+        ])
+    elif mode == 'yoloxpose':
+        print("Preparing for YOLOX-Pose processing...")
+        configurations.extend([
+            f"{mode}_t_4xb64-300e_{dataset_name}-416.py",
+            f"{mode}_s_8xb32-300e_{dataset_name}-640.py",
+            f"{mode}_m_8xb32-300e_{dataset_name}-640.py",
+            f"{mode}_l_8xb32-300e_{dataset_name}-640.py"
+        ])
+    elif mode == 'vitpose':
+        print("Preparing for ViTPose processing...")
+        configurations.extend([
+            f"configs/animal_2d_keypoint/topdown_heatmap/{dataset_name}/td-hm_ViTPose-small-simple_8xb64-210e_{dataset_name}-256x256.py",
+            f"configs/animal_2d_keypoint/topdown_heatmap/{dataset_name}/td-hm_ViTPose-base-simple_8xb64-210e_{dataset_name}-256x256.py",
+            f"configs/animal_2d_keypoint/topdown_heatmap/{dataset_name}/td-hm_ViTPose-large-simple_8xb64-210e_{dataset_name}-256x256.py",
+            f"configs/animal_2d_keypoint/topdown_heatmap/{dataset_name}/td-hm_ViTPose-huge-simple_8xb64-210e_{dataset_name}-256x256.py",
+            f"configs/animal_2d_keypoint/topdown_heatmap/{dataset_name}/td-hm_ViTPose-small_8xb64-210e_{dataset_name}-256x256.py",
+            f"configs/animal_2d_keypoint/topdown_heatmap/{dataset_name}/td-hm_ViTPose-base_8xb64-210e_{dataset_name}-256x256.py",
+            f"configs/animal_2d_keypoint/topdown_heatmap/{dataset_name}/td-hm_ViTPose-large_8xb64-210e_{dataset_name}-256x256.py",
+            f"configs/animal_2d_keypoint/topdown_heatmap/{dataset_name}/td-hm_ViTPose-huge_8xb64-210e_{dataset_name}-256x256.py"
+        ])
+    elif mode == 'animalvitpose':
+        print("Preparing for AnimalViTPose processing...")
+        configurations.extend([
+            f"configs/animal_2d_keypoint/animalvitpose/{dataset_name}/animalvitpose-small_8xb64-210e_{dataset_name}-256x256.py",
+            f"configs/animal_2d_keypoint/animalvitpose/{dataset_name}/animalvitpose-base_8xb64-210e_{dataset_name}-256x256.py",
+            f"configs/animal_2d_keypoint/animalvitpose/{dataset_name}/animalvitpose-large_8xb64-210e_{dataset_name}-256x256.py",
+            f"configs/animal_2d_keypoint/animalvitpose/{dataset_name}/animalvitpose-huge_8xb64-210e_{dataset_name}-256x256.py"
+        ])
+    else:
+        print(f"Unsupported mode: {mode}. Please choose a valid mode.")
+        return None
+
+    return configurations
+
+
+def build_output_dir(
+    base_dir, 
+    optical_field_sizes=None, 
+    sub_optical_field_sizes=None, 
+    window_size=None, 
+    seed=None, 
+    inverse=False, 
+    imgsz_hadamard=None,
+    aliasing=False
+):
+    """Build the save directory based on the provided arguments."""
+    base_dir = f"{base_dir}"
+    
+    if optical_field_sizes is not None:
+        base_dir += f"-{optical_field_sizes}x{optical_field_sizes}"
+    
+    if sub_optical_field_sizes is not None:
+        base_dir += f"-{sub_optical_field_sizes}x{sub_optical_field_sizes}"
+    
+    if window_size is not None:
+        base_dir += f"-{window_size[0]}x{window_size[1]}"
+
+    if inverse:
+        base_dir += "-inverse"
+    
+    if aliasing:
+        base_dir += "-aliasing"
+    
+    if imgsz_hadamard is not None:
+        base_dir += f"-{imgsz_hadamard}"
+    
+    if seed is not None:
+        base_dir += f"-{seed}"
+    
+    return base_dir
+
+def rename_dataset_directory(original_name, temp_name):
+    """Rename the dataset directory."""
+    if os.path.exists(original_name):
+        os.rename(original_name, temp_name)
+    else:
+        print(f"Dataset directory {original_name} does not exist.")
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Train models with different configurations.')
+    parser.add_argument('--dataset', type=str, default='ap10k', help='Dataset name')
+    parser.add_argument('--gpu', type=str, default='0', help='GPU number to use')
+    parser.add_argument('--mode', type=str, default='topdown_heatmap', choices=[
+        'bottomup', 'rtmo', 'rtmpose', 'topdown_heatmap', 'topdown_others', 'yoloxpose', 'vitpose', 'animalvitpose'
+    ], help='Mode of operation')
+    parser.add_argument("--optical-field-size", type=int, default=None, help="Optical field size for the entire image.")
+    parser.add_argument("--sub-optical-field-size", type=int, default=None, help="Optical field size for sub-regions of the image.")
+    parser.add_argument("--window-size", nargs=2, type=int, default=None, help="Window size for sub-regions of the image.")
+    parser.add_argument("--inverse", action="store_true", help="Order the images by their size before splitting into sub-regions.")
+    parser.add_argument("--imgsz-hadamard", type=int, default=None, help="Image size for the Hadamard transform. If not provided, it will be set to imgsz.")
+    parser.add_argument("--aliasing", action="store_true", help="Use aliasing for the Hadamard transform.")
+
+    return parser.parse_args()
+
+def find_latest_checkpoint(directory):
+    checkpoints = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.pth')]
+    if checkpoints:
+        return max(checkpoints, key=os.path.getmtime)
+    else:
+        return None
+
+def main():
+    args = parse_arguments()
+
+    DATASET_NAME = args.dataset
+    GPU_NUMBER = args.gpu
+    MODE = args.mode
+    OPTICAL_FIELD_SIZE = args.optical_field_size
+    SUB_OPTICAL_FIELD_SIZE = args.sub_optical_field_size
+    WINDOW_SIZE = args.window_size
+    INVERSE = args.inverse
+    IMGSZ_HADAMARD = args.imgsz_hadamard
+    ALIASING = args.aliasing
+
+
+    original_dataset_dir = build_output_dir(
+            f"data/{DATASET_NAME}/images",
+            optical_field_sizes=OPTICAL_FIELD_SIZE,
+            sub_optical_field_sizes=SUB_OPTICAL_FIELD_SIZE,
+            window_size=WINDOW_SIZE,
+            inverse=INVERSE,
+            imgsz_hadamard=IMGSZ_HADAMARD,
+            aliasing=ALIASING
+        )
+    _, split_original_dataset_dir = original_dataset_dir.split("-", 1)
+    BASE_WORK_CONFIG_PATH = f"work_dirs/{MODE}/{DATASET_NAME}-{split_original_dataset_dir}"
+
+    temp_dataset_dir = f"data/{DATASET_NAME}/images"
+
+    rename_dataset_directory(original_dataset_dir, temp_dataset_dir)
+
+    CUDA_COMMAND = f"CUDA_VISIBLE_DEVICES={GPU_NUMBER}" 
+
+    configurations = prepare_configurations(MODE, DATASET_NAME)
+    
+    try:
+        for config in configurations:
+            config_name = os.path.basename(config).split('.')[0]
+            work_dir = os.path.join(BASE_WORK_CONFIG_PATH, config_name)
+            config_path = os.path.join(BASE_WORK_CONFIG_PATH, config)
+            print(config_path)
+            checkpoint_path = find_latest_checkpoint(work_dir)
+            if checkpoint_path is None:
+               print(f"No checkpoint found for {work_dir}")
+               continue
+            command = f"{CUDA_COMMAND} python tools/test.py  {config_path} {checkpoint_path}"
+            print(f"Executing: {command}")
+            subprocess.run(command, shell=True)
+    finally:
+        rename_dataset_directory(temp_dataset_dir, original_dataset_dir)
+
+if __name__ == '__main__':
+    main()
