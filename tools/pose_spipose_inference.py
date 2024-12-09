7f4b1f54a24c751a6e95d96a0cb1588c8e64b0c5
Adam Wiveion <wux024@nenu.edu.cn>
2024-12-09 10:25:28 +0800

    update

--- tools/pose_spipose_inference.py
+++ tools/pose_spipose_inference.py
@@ -1,222 +1,182 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from argparse import ArgumentParser
-from typing import Dict
+from mmpose.apis.inferencers import MMPoseInferencer
+import argparse
+import os
+import subprocess
 
-from mmpose.apis.inferencers import MMPoseInferencer, get_model_aliases
+def build_output_dir(
+    base_dir, 
+    optical_field_sizes=None, 
+    sub_optical_field_sizes=None, 
+    window_size=None, 
+    hadamard_seed=None, 
+    inverse=False, 
+    imgsz_hadamard=None,
+    aliasing=False
+):
+    """Build the save directory based on the provided arguments."""
+    base_dir = f"{base_dir}"
+    
+    if optical_field_sizes is not None:
+        base_dir += f"-{optical_field_sizes}x{optical_field_sizes}"
+    
+    if sub_optical_field_sizes is not None:
+        base_dir += f"-{sub_optical_field_sizes}x{sub_optical_field_sizes}"
+    
+    if window_size is not None:
+        base_dir += f"-{window_size[0]}x{window_size[1]}"
 
-filter_args = dict(bbox_thr=0.3, nms_thr=0.3, pose_based_nms=False)
-POSE2D_SPECIFIC_ARGS = dict(
-    yoloxpose=dict(bbox_thr=0.01, nms_thr=0.65, pose_based_nms=True),
-    rtmo=dict(bbox_thr=0.1, nms_thr=0.65, pose_based_nms=True),
-)
+    if inverse:
+        base_dir += "-inverse"
+    
+    if aliasing:
+        base_dir += "-aliasing"
+    
+    if imgsz_hadamard is not None:
+        base_dir += f"-{imgsz_hadamard}"
+    
+    if hadamard_seed is not None:
+        base_dir += f"-{hadamard_seed}"
+    
+    return base_dir
 
+def rename_dataset_directory(original_name, temp_name):
+    """Rename the dataset directory."""
+    if os.path.exists(original_name):
+        os.rename(original_name, temp_name)
+    else:
+        print(f"Dataset directory {original_name} does not exist.")
 
-def parse_args():
-    parser = ArgumentParser()
-    parser.add_argument(
-        'inputs',
-        type=str,
-        nargs='?',
-        help='Input image/video path or folder path.')
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Train models with different configurations.')
+    parser.add_argument('--source', type=str, default='None', help='Source directory of the images or video.')
+    parser.add_argument('--dataset', type=str, default='ap10k', help='Dataset name')
+    parser.add_argument('--scale', type=str, nargs='+', default=['s'], help='Scale of the model, can be s(mall), b(ase), l(arge), and h(uge).')
+    parser.add_argument("--optical-field-size", type=int, default=None, help="Optical field size for the entire image.")
+    parser.add_argument("--sub-optical-field-size", type=int, default=None, help="Optical field size for sub-regions of the image.")
+    parser.add_argument("--window-size", nargs=2, type=int, default=None, help="Window size for sub-regions of the image.")
+    parser.add_argument("--inverse", action="store_true", help="Order the images by their size before splitting into sub-regions.")
+    parser.add_argument("--imgsz-hadamard", type=int, default=None, help="Image size for the Hadamard transform. If not provided, it will be set to imgsz.")
+    parser.add_argument("--aliasing", action="store_true", help="Use aliasing for the Hadamard transform.")
+    parser.add_argument("--hadamard-seed", type=int, default=None, help="Seed for the Hadamard transform.")
+    
+    # inference options
+    parser.add_argument('--device', type=str, default='None', help='Device used for inference.')
+    parser.add_argument('--show', action='store_true', help='Show visualizations.')
+    parser.add_argument('--radius', type=int, default=4, help='Keypoint radius for visualization.')
+    parser.add_argument('--thickness', type=int, default=1, help='Link thickness for visualization.')
+    parser.add_argument('--kpt-thr', type=float, default=0.3, help='Keypoint score threshold for visualization.')
+    parser.add_argument('--draw-bbox', action='store_true', help='Draw bounding box for visualization.')
+    parser.add_argument('--draw-heatmap', action='store_true', help='Draw heatmap for visualization.')
+    parser.add_argument('--black-background', action='store_true', help='Use black background for visualization.')
+    parser.add_argument('--return-vis', action='store_true', help='Return visualizations.')
+    parser.add_argument('--vis-out-dir', type=str, default='None', help='Directory to save visualizations.')
+    parser.add_argument('--return-datasamples', action='store_true', help='Return data samples.')
+    parser.add_argument('--pred-out-dir', type=str, default='None', help='Directory to save predictions.')
+    parser.add_argument('--out-dir', type=str, default='None', help='Directory to save output files.')
 
-    # init args
-    parser.add_argument(
-        '--pose2d',
-        type=str,
-        default=None,
-        help='Pretrained 2D pose estimation algorithm. It\'s the path to the '
-        'config file or the model name defined in metafile.')
-    parser.add_argument(
-        '--pose2d-weights',
-        type=str,
-        default=None,
-        help='Path to the custom checkpoint file of the selected pose model. '
-        'If it is not specified and "pose2d" is a model name of metafile, '
-        'the weights will be loaded from metafile.')
-    parser.add_argument(
-        '--pose3d',
-        type=str,
-        default=None,
-        help='Pretrained 3D pose estimation algorithm. It\'s the path to the '
-        'config file or the model name defined in metafile.')
-    parser.add_argument(
-        '--pose3d-weights',
-        type=str,
-        default=None,
-        help='Path to the custom checkpoint file of the selected pose model. '
-        'If it is not specified and "pose3d" is a model name of metafile, '
-        'the weights will be loaded from metafile.')
-    parser.add_argument(
-        '--det-model',
-        type=str,
-        default=None,
-        help='Config path or alias of detection model.')
-    parser.add_argument(
-        '--det-weights',
-        type=str,
-        default=None,
-        help='Path to the checkpoints of detection model.')
-    parser.add_argument(
-        '--det-cat-ids',
-        type=int,
-        nargs='+',
-        default=0,
-        help='Category id for detection model.')
-    parser.add_argument(
-        '--scope',
-        type=str,
-        default='mmpose',
-        help='Scope where modules are defined.')
-    parser.add_argument(
-        '--device',
-        type=str,
-        default=None,
-        help='Device used for inference. '
-        'If not specified, the available device will be automatically used.')
-    parser.add_argument(
-        '--show-progress',
-        action='store_true',
-        help='Display the progress bar during inference.')
+    return parser.parse_args()
 
-    # The default arguments for prediction filtering differ for top-down
-    # and bottom-up models. We assign the default arguments according to the
-    # selected pose2d model
-    args, _ = parser.parse_known_args()
-    for model in POSE2D_SPECIFIC_ARGS:
-        if args.pose2d is not None and model in args.pose2d:
-            filter_args.update(POSE2D_SPECIFIC_ARGS[model])
-            break
-
-    # call args
-    parser.add_argument(
-        '--show',
-        action='store_true',
-        help='Display the image/video in a popup window.')
-    parser.add_argument(
-        '--draw-bbox',
-        action='store_true',
-        help='Whether to draw the bounding boxes.')
-    parser.add_argument(
-        '--draw-heatmap',
-        action='store_true',
-        default=False,
-        help='Whether to draw the predicted heatmaps.')
-    parser.add_argument(
-        '--bbox-thr',
-        type=float,
-        default=filter_args['bbox_thr'],
-        help='Bounding box score threshold')
-    parser.add_argument(
-        '--nms-thr',
-        type=float,
-        default=filter_args['nms_thr'],
-        help='IoU threshold for bounding box NMS')
-    parser.add_argument(
-        '--pose-based-nms',
-        type=lambda arg: arg.lower() in ('true', 'yes', 't', 'y', '1'),
-        default=filter_args['pose_based_nms'],
-        help='Whether to use pose-based NMS')
-    parser.add_argument(
-        '--kpt-thr', type=float, default=0.3, help='Keypoint score threshold')
-    parser.add_argument(
-        '--tracking-thr', type=float, default=0.3, help='Tracking threshold')
-    parser.add_argument(
-        '--use-oks-tracking',
-        action='store_true',
-        help='Whether to use OKS as similarity in tracking')
-    parser.add_argument(
-        '--disable-norm-pose-2d',
-        action='store_true',
-        help='Whether to scale the bbox (along with the 2D pose) to the '
-        'average bbox scale of the dataset, and move the bbox (along with the '
-        '2D pose) to the average bbox center of the dataset. This is useful '
-        'when bbox is small, especially in multi-person scenarios.')
-    parser.add_argument(
-        '--disable-rebase-keypoint',
-        action='store_true',
-        default=False,
-        help='Whether to disable rebasing the predicted 3D pose so its '
-        'lowest keypoint has a height of 0 (landing on the ground). Rebase '
-        'is useful for visualization when the model do not predict the '
-        'global position of the 3D pose.')
-    parser.add_argument(
-        '--num-instances',
-        type=int,
-        default=1,
-        help='The number of 3D poses to be visualized in every frame. If '
-        'less than 0, it will be set to the number of pose results in the '
-        'first frame.')
-    parser.add_argument(
-        '--radius',
-        type=int,
-        default=3,
-        help='Keypoint radius for visualization.')
-    parser.add_argument(
-        '--thickness',
-        type=int,
-        default=1,
-        help='Link thickness for visualization.')
-    parser.add_argument(
-        '--skeleton-style',
-        default='mmpose',
-        type=str,
-        choices=['mmpose', 'openpose'],
-        help='Skeleton style selection')
-    parser.add_argument(
-        '--black-background',
-        action='store_true',
-        help='Plot predictions on a black image')
-    parser.add_argument(
-        '--vis-out-dir',
-        type=str,
-        default='',
-        help='Directory for saving visualized results.')
-    parser.add_argument(
-        '--pred-out-dir',
-        type=str,
-        default='',
-        help='Directory for saving inference results.')
-    parser.add_argument(
-        '--show-alias',
-        action='store_true',
-        help='Display all the available model aliases.')
-
-    call_args = vars(parser.parse_args())
-
-    init_kws = [
-        'pose2d', 'pose2d_weights', 'scope', 'device', 'det_model',
-        'det_weights', 'det_cat_ids', 'pose3d', 'pose3d_weights',
-        'show_progress'
-    ]
-    init_args = {}
-    for init_kw in init_kws:
-        init_args[init_kw] = call_args.pop(init_kw)
-
-    display_alias = call_args.pop('show_alias')
-
-    return init_args, call_args, display_alias
-
-
-def display_model_aliases(model_aliases: Dict[str, str]) -> None:
-    """Display the available model aliases and their corresponding model
-    names."""
-    aliases = list(model_aliases.keys())
-    max_alias_length = max(map(len, aliases))
-    print(f'{"ALIAS".ljust(max_alias_length+2)}MODEL_NAME')
-    for alias in sorted(aliases):
-        print(f'{alias.ljust(max_alias_length+2)}{model_aliases[alias]}')
-
+def find_latest_checkpoint(directory):
+    checkpoints = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.pth')]
+    if checkpoints:
+        return max(checkpoints, key=os.path.getmtime)
+    else:
+        return None
 
 def main():
-    init_args, call_args, display_alias = parse_args()
-    if display_alias:
-        model_alises = get_model_aliases(init_args['scope'])
-        display_model_aliases(model_alises)
-    else:
-        inferencer = MMPoseInferencer(**init_args)
-        for _ in inferencer(**call_args):
-            pass
+    args = parse_arguments()
 
+    SOURCE_DIR = args.source
+    DATASET_NAME = args.dataset
+    SCALE = args.scale
+    OPTICAL_FIELD_SIZE = args.optical_field_size
+    SUB_OPTICAL_FIELD_SIZE = args.sub_optical_field_size
+    WINDOW_SIZE = args.window_size
+    INVERSE = args.inverse
+    IMGSZ_HADAMARD = args.imgsz_hadamard
+    ALIASING = args.aliasing
+    HADAMARD_SEED = args.hadamard_seed
+
+    DEVICE = args.device
+    SHOW = args.show
+    RADIUS = args.radius
+    THICKNESS = args.thickness
+    KPT_THR = args.kpt_thr    
+    DRAW_BBOX = args.draw_bbox
+    DRAW_HEATMAP = args.draw_heatmap
+    BLACK_BACKGROUND = args.black_background
+    RETURN_VIS = args.return_vis
+    RETURN_DATASAMPLES = args.return_datasamples
+
+    scale_configs = {
+        "s": f"spipose-small_8xb64-210e_{DATASET_NAME}-256x256.py",
+        "b": f"spipose-base_8xb64-210e_{DATASET_NAME}-256x256.py",
+        "l": f"spipose-large_8xb64-210e_{DATASET_NAME}-256x256.py",
+        "h": f"spipose-huge_8xb64-210e_{DATASET_NAME}-256x256.py"
+    }
+    configurations = []
+    for scale in SCALE:
+        if scale not in scale_configs:
+            print(f"Invalid scale: {scale}")
+            continue
+        configurations.append(scale_configs[scale])
+    
+    if not configurations:
+        print("No valid scale provided.")
+        return
+
+    original_dataset_dir = build_output_dir(
+            f"data/{DATASET_NAME}/images",
+            optical_field_sizes=OPTICAL_FIELD_SIZE,
+            sub_optical_field_sizes=SUB_OPTICAL_FIELD_SIZE,
+            window_size=WINDOW_SIZE,
+            inverse=INVERSE,
+            imgsz_hadamard=IMGSZ_HADAMARD,
+            aliasing=ALIASING,
+            hadamard_seed=HADAMARD_SEED
+        )
+    _, split_original_dataset_dir = original_dataset_dir.split("-", 1)
+    BASE_WORK_CONFIG_PATH = f"work_dirs/spipose/{DATASET_NAME}-{split_original_dataset_dir}"
+
+    temp_dataset_dir = f"data/{DATASET_NAME}/images"
+
+    if SOURCE_DIR == 'None':
+        SOURCE_DIR = os.path.join(temp_dataset_dir, "test")
+
+    rename_dataset_directory(original_dataset_dir, temp_dataset_dir)
+    
+    try:
+        for config in configurations:
+            config_name = config.split('.')[0]
+            work_dir = os.path.join(BASE_WORK_CONFIG_PATH, config_name)
+            config_path = os.path.join(BASE_WORK_CONFIG_PATH, config_name, f"{config_name}.py")
+            checkpoint_path = find_latest_checkpoint(work_dir)
+            if checkpoint_path is None:
+               print(f"No checkpoint found for {work_dir}")
+               continue
+            inferencer = MMPoseInferencer(
+                pose2d = config_path,
+                pose2d_weights = checkpoint_path,
+                device = DEVICE,
+                show = SHOW,
+                radius = RADIUS,
+                thickness = THICKNESS,
+                kpt_thr = KPT_THR,
+                draw_bbox = DRAW_BBOX,
+                draw_heatmap = DRAW_HEATMAP,
+                black_background = BLACK_BACKGROUND,
+                return_vis = RETURN_VIS,
+                vis_out_dir = os.path.join(work_dir, "visualizations"),
+                return_datasamples = RETURN_DATASAMPLES,
+                pred_out_dir = os.path.join(work_dir, "predictions")
+            )
+            result_generator = inferencer(SOURCE_DIR)
+            for _ in result_generator:
+                pass
+
+    finally:
+        rename_dataset_directory(temp_dataset_dir, original_dataset_dir)
 
 if __name__ == '__main__':
     main()
+
