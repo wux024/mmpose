8e550f984c15577c0ddbdf6218a907c12b91396a
Adam Wiveion <wux024@nenu.edu.cn>
2025-07-31 14:28:06 +0800

    update

--- configs/animal_2d_keypoint/animalvitpose/ap10k_ab/swin-l-p4-w7_8xb64-210e_ap10k-256x256.py
+++ configs/animal_2d_keypoint/animalvitpose/ap10k_ab/swin-l-p4-w7_8xb64-210e_ap10k-256x256.py
@@ -1,7 +1,7 @@
 _base_ = ['../../../_base_/default_runtime.py']
 
 # runtime
-train_cfg = dict(max_epochs=210, val_interval=10)
+train_cfg = dict(max_epochs=500, val_interval=10)
 
 # optimizer
 optim_wrapper = dict(
@@ -26,8 +26,8 @@ param_scheduler = [
     dict(
         type='MultiStepLR',
         begin=0,
-        end=210,
-        milestones=[170, 200],
+        end=500,
+        milestones=[460, 490],
         gamma=0.1,
         by_epoch=True)
 ]
