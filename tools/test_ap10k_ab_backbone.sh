2665082dcdc37097fd724987ba534a96c7401e7e
Adam Wiveion <wux024@nenu.edu.cn>
2025-08-01 14:21:43 +0800

    update

--- tools/test_ap10k_ab_backbone.sh
+++ tools/test_ap10k_ab_backbone.sh
@@ -18,8 +18,10 @@ configurations=(
 # Execute training based on the selected mode's configurations
 for config in "${configurations[@]}"; do
     # Extracting configuration file name without path and extension for work_dir
-    config_name="${config%.*}"
-    config_path=${BASE_CONFIG_PATH}/${config_name}/${config}
-    checkpoint_path=$(find "$BASE_CONFIG_PATH/$config_name" -type f -name "*.pth" | sort -r | head -n 1)
+    config_name=$(basename -- "$config")
+    config_name="${config_name%.*}" # Removing the file extension
+    work_dir="./work_dirs/animalvitpose/ap10k_ab/${config_name}"
+    config_path="./work_dirs/animalvitpose/ap10k_ab/${config_name}/${config_name}.py"
+    checkpoint_path=$(find "$work_dir" -type f -name "*.pth" | sort -r | head -n 1)
     env $CUDA_COMMAND python tools/test.py "$config_path" "$checkpoint_path"
 done
